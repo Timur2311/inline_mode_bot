191aa9e38fd2e382fb90fbd9660281c26f2108a2
Timur2311 <zayniddinov.programming@gmail.com>
2022-07-22 06:12:37 +0500

    boiler plate

--- tgbot/handlers/onboarding/handlers.py
+++ tgbot/handlers/onboarding/handlers.py
@@ -17,9 +17,10 @@ def command_start(update: Update, context: CallbackContext) -> None:
         text = static_text.start_created.format(first_name=u.first_name)
     else:
         text = static_text.start_not_created.format(first_name=u.first_name)
+        
+    text += "Bot ni inline rejimida ishga tushirish uchun boshqa chatga kirib bot username ni kiriting!"
 
-    update.message.reply_text(text=text,
-                              reply_markup=make_keyboard_for_start_command())
+    update.message.reply_text(text=text)
 
 
 def secret_level(update: Update, context: CallbackContext) -> None:
